ff388d978b5b5550d454cce4b8a936584b229250
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-05-03 22:41:52 +0530

    Setting up

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from typing import List, Optional
+from db import users
 
+class Notification(BaseModel):
+    author: str,
+    description: str
+    
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static",StaticFiles(directory="static"), name="static")
